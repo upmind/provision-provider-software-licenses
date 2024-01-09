e8f9fd6babfe2bd587055be8a8cd871776cb71fc
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2024-01-09 15:52:58 +0000

    assert that reissue is not supported

---
+++ tests/Unit/Providers/CPanel/ProviderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Providers\CPanel;
+
+use PHPUnit\Framework\TestCase;
+use Upmind\ProvisionBase\Exception\ProvisionFunctionError;
+use Upmind\ProvisionProviders\SoftwareLicenses\Data\ReissueParams;
+use Upmind\ProvisionProviders\SoftwareLicenses\Providers\CPanel\Data\Configuration;
+use Upmind\ProvisionProviders\SoftwareLicenses\Providers\CPanel\Provider;
+
+class ProviderTest extends TestCase
+{
+    private ReissueParams $reissueParams;
+    private Provider $provider;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->reissueParams = $this->createMock(ReissueParams::class);
+
+        $configuration = $this->createMock(Configuration::class);
+        $this->provider = new Provider($configuration);
+    }
+
+    public function testReissueIsNotSupported(): void
+    {
+        $this->expectException(ProvisionFunctionError::class);
+
+        $this->provider->reissue($this->reissueParams);
+    }
+
+
+}
