0c3e7e3ca8889dc5c227d1dd72e82acfcf46e96e
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2024-01-17 13:08:45 +0000

    cast to boolean instead of !! to get same result

--- src/Providers/Generic/Provider.php
+++ src/Providers/Generic/Provider.php
@@ -225,7 +225,7 @@ class Provider extends Category implements ProviderInterface
     {
         return new Client([
             RequestOptions::HTTP_ERRORS => false,
-            'handler' => $this->getGuzzleHandlerStack(!!$this->configuration->debug),
+            'handler' => $this->getGuzzleHandlerStack((bool) $this->configuration->debug),
         ]);
     }
 }
